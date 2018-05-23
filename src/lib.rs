a7674b8e3aa931e85a42e38e0d287c2c76e59a40
Hakan Dingenc <hakan.dingenc@u.northwestern.edu>
2018-05-23 00:11:12 -0500

    Receiver kind of working

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,65 @@ extern crate url;
 use futures::{Future, Stream};
 use hyper::Client;
 use hyper::header::{ContentLength, ContentType};
-use hyper::{Method, Request};
+use hyper::server::{Http, Response, Service};
+use hyper::{Method, Request, StatusCode};
 use hyper_tls::HttpsConnector;
+use std::collections::HashMap;
 use std::io::{self, Write};
 use tokio_core::reactor::Core;
-use url::{Host, ParseError, Url};
+use url::{form_urlencoded, Host, ParseError, Url};
+
+const NUM_THREADS: usize = 4;
 
 pub struct Forwarder {
     payload: String,
     webhook: hyper::Uri,
+}
+
+pub struct Receiver;
+
+impl Service for Receiver {
+    // boilerplate hooking up hyper's server types
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    // The future representing the eventual Response your call will
+    // resolve to. This can change to whatever Future you need.
+    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn call(&self, req: Request) -> Self::Future {
+        let mut response = Response::new();
+
+        match (req.method(), req.path()) {
+            (&Method::Get, "/") => {
+                response.set_body("This server expects POST requests to /");
+            }
+            (&Method::Post, "/") => {
+                Box::new(req.body().concat2().map(|b| {
+                    let my_string = String::from_utf8_lossy(b.as_ref());
+                    println!("{:?}", my_string);
+
+                    let body = format!("Hello {}, your number is", my_string);
+                    // return Box::new(futures::future::ok(
+                    //     Response::new()
+                    //         .with_header(ContentLength(body.len() as u64))
+                    //         .with_body(body),
+                    // ));
+                }));
+                println!("ABC");
+                // let my_forwarder = Forwarder::new(
+                //     my_string,
+                //     "https://hooks.slack.com/services/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",
+                // );
+                // my_forwarder.send();
+            }
+            _ => {
+                response.set_status(StatusCode::NotFound);
+            }
+        };
+
+        Box::new(futures::future::ok(response))
+    }
 }
 
 impl Forwarder {
@@ -31,7 +81,7 @@ impl Forwarder {
         let mut core = Core::new().unwrap();
         let handle = core.handle();
         let client = Client::configure()
-            .connector(HttpsConnector::new(4, &handle).unwrap())
+            .connector(HttpsConnector::new(NUM_THREADS, &handle).unwrap())
             .build(&handle);
         let mut req = Request::new(Method::Post, self.webhook.clone());
         req.headers_mut().set(ContentType::json());
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
+extern crate hyper;
 extern crate rson;
+use hyper::server::Http;
 use rson::*;
 
 fn main() {
-    let my_string = "{\"text\":\"This is a line of text.\nAnd this is another one.\"}".to_string();
-    let my_forwarder = Forwarder::new(
-        my_string,
-        "https://hooks.slack.com/services/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",
-    );
-    my_forwarder.send();
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    let server = Http::new().bind(&addr, || Ok(Receiver)).unwrap();
+    server.run().unwrap();
 }
