ee4837d533412ff9ea13ac067b582d1c3ff5b250
Mateusz Ryczek <skyfused@gmail.com>
2018-06-07 15:39:02 -0500

    Add docs to pages branch

---
+++ target/doc/implementors/core/marker/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rson"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"enum\" href=\"rson/enum.Command.html\" title=\"enum rson::Command\">Command</a>",synthetic:true,types:["rson::Command"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"rson/hostmap/struct.HostMap.html\" title=\"struct rson::hostmap::HostMap\">HostMap</a>",synthetic:true,types:["rson::hostmap::HostMap"]},{text:"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"rson/server/struct.Server.html\" title=\"struct rson::server::Server\">Server</a>",synthetic:true,types:["rson::server::Server"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
