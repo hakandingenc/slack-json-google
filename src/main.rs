0205b4eda230c734b516ade1b36bb1d493aa2bd3
Hakan Dingenc <hakan.dingenc@u.northwestern.edu>
2018-06-07 10:07:35 -0500

    Handle start_server return value

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,8 @@ fn main() {
     let addr = "127.0.0.1:3000".parse().unwrap();
     let filename = Path::new("mappings.json");
     let response_to_slack = "{\"text\": \"✅Your request has been received!\"}";
-    start_server(addr, filename, response_to_slack);
+    match start_server(addr, filename, response_to_slack) {
+        Ok(_) => {}
+        Err(error) => panic!(error),
+    }
 }
